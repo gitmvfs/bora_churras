411ef18cd3d3a2270ba72251b6e086afb1e7d882
gitmvfs <marcossantos8002@gmail.com>
2024-04-28 20:23:42 -0300

    test: tela sendo usada para teste de component

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,39 +1,38 @@
-// import Header from "../components/header";
-import { View, StyleSheet , Dimensions, StatusBar} from "react-native";
-import Titulo from "../components/titulo";
-import ToggleComida from "../components/toggleComida";
-import { BotaoConsumo, BotaoProximaTela, BotaoTelaAnterior } from "../components/botoesNav";
-const screenWidth = Dimensions.get('window').width;
-const screenHeight = Dimensions.get('window').height;
+// // import Header from "../components/header";
+// import { View, StyleSheet , Dimensions, StatusBar} from "react-native";
+// import ToggleComida from "../components/toggleComida";
+// import { BotaoConsumo, BotaoProximaTela, BotaoTelaAnterior } from "../components/botoesNav";
+// const screenWidth = Dimensions.get('window').width;
+// const screenHeight = Dimensions.get('window').height;
 
-function HomeScreen() {
+// function HomeScreen() {
 
-    return (
-        <View style={styles.HomeScreen}>
-            <StatusBar hidden={true}/>
-            <Titulo nome="Titulo" />
-            <ToggleComida nome="Picanha" />
-            <BotaoProximaTela nome="Confirmar "/>
-            <BotaoTelaAnterior nome="Anterior"/>
-            <BotaoConsumo/>
-        </View>
-    )
-}
+//     return (
+//         <View style={styles.HomeScreen}>
+//             <StatusBar hidden={true}/>
+            
+//             <ToggleComida nome="Picanha" />
+//             <BotaoProximaTela nome="Confirmar "/>
+//             <BotaoTelaAnterior nome="Anterior"/>
+//             <BotaoConsumo/>
+//         </View>
+//     )
+// }
 
-const styles = StyleSheet.create({
+// const styles = StyleSheet.create({
 
-    HomeScreen:{
-        paddingTop: 30,
-        backgroundColor:"#260101",
-        minHeight: screenHeight,
-        width: screenWidth,
-        maxWidth: screenWidth,
-        position:"absolute",
-        top:"0",
-        left:0
-    }
+//     HomeScreen:{
+//         paddingTop: 30,
+//         backgroundColor:"#260101",
+//         minHeight: screenHeight,
+//         width: screenWidth,
+//         maxWidth: screenWidth,
+//         position:"absolute",
+//         top:"0",
+//         left:0
+//     }
 
-})
+// })
 
 
-export default HomeScreen;
+// export default HomeScreen;
