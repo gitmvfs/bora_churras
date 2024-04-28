a29656cba143686cd2c4cbe7bbcf7ea770210f04
gitmvfs <marcossantos8002@gmail.com>
2024-04-28 20:22:36 -0300

    feat: adicionado roda de produtoSelect

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen'; // Importe o componente da sua tela inicial
+import { ProdutoSelect } from './src/screens/ProdutoSelect';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { limparSelecaoProdutos } from './src/localStorage/localStorage';
+// limparSelecaoProdutos()
+
 
 const Stack = createStackNavigator();
 
@@ -10,9 +15,14 @@ function App() {
     
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen
+        {/* <Stack.Screen
           name="Home"
           component={HomeScreen}
+          options={{ headerShown: false }} // Define headerShown como false para esconder o cabeçalho
+        /> */}
+        <Stack.Screen
+          name="ProdutoSelect"
+          component={ProdutoSelect}
           options={{ headerShown: false }} // Define headerShown como false para esconder o cabeçalho
         />
       </Stack.Navigator>
