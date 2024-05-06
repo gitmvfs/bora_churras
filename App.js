9bca2782989a18756783845381de6dc58ec2d9e1
gitmvfs <marcossantos8002@gmail.com>
2024-05-06 13:26:54 -0300

    fix: descomentado rotas

--- App.js
+++ App.js
@@ -15,9 +15,9 @@ function App() {
       <StatusBar hidden />
       <NavigationContainer>
         <Stack.Navigator initialRouteName='HomeScreen' screenOptions={{ headerShown: false }}>
-          {/* <Stack.Screen name="Home" component={HomeScreen} /> */}
-          {/* <Stack.Screen name="ProdutoScreen" component={ProdutoScreen} /> */}
-          {/* <Stack.Screen name="ConsumoScreen" component={ConsumoScreen} /> */}
+          <Stack.Screen name="Home" component={HomeScreen} />
+          <Stack.Screen name="ProdutoScreen" component={ProdutoScreen} />
+          <Stack.Screen name="ConsumoScreen" component={ConsumoScreen} />
           <Stack.Screen name="MercadoScreen" component={MercadoScreen} />
         </Stack.Navigator>
       </NavigationContainer>
