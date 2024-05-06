a02ae487f68d09329e680686b8610dc97bad68d1
gitmvfs <marcossantos8002@gmail.com>
2024-05-06 12:42:55 -0300

    feat: componente e tela consumo

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './src/screens/HomeScreen'; // Importe o componente da sua tela inicial
 import { ProdutoScreen } from './src/screens/ProdutoScreen';
+import { ConsumoScreen } from './src/screens/ConsumoScreen';
 import { StatusBar } from 'react-native'; // Importe o StatusBar aqui
 // limparSelecaoProdutos()
 const Stack = createStackNavigator();
@@ -15,6 +16,7 @@ function App() {
         <Stack.Navigator initialRouteName='HomeScreen' screenOptions={{ headerShown: false }}>
           <Stack.Screen name="Home" component={HomeScreen} />
           <Stack.Screen name="ProdutoScreen" component={ProdutoScreen} />
+          <Stack.Screen name="ConsumoScreen" component={ConsumoScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
